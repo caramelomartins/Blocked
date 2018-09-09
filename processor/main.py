2be8e63ea8518a59dc8e5400bf4057ba80770271
Hugo Martins <caramelo.martins@gmail.com>
2018-09-09 20:01:20 +0100

    Add argument to processor/main.py

--- processor/main.py
+++ processor/main.py
@@ -3,6 +3,7 @@ main.py
 
 This module implements the main function that executes the processor.
 """
+import argparse
 import logging
 
 from sawtooth_sdk.processor.core import TransactionProcessor
@@ -11,14 +12,14 @@ from sawtooth_sdk.processor.log import init_console_logging
 import handler
 
 
-def main():
+def main(url):
     try:
         init_console_logging(verbose_level=2)
         logger = logging.getLogger(__name__)
 
         # Start a transaction processor, listening to a given host.
         # TODO: Parameterize this.
-        processor = TransactionProcessor(url='tcp://localhost:4004')
+        processor = TransactionProcessor(url=url)
 
         # Add custom handler to our TP.
         blocked_handler = handler.BlockedHandler()
@@ -36,4 +37,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-C', default='tcp://localhost:4004')
+    args = parser.parse_args()
+    main(args.C)
